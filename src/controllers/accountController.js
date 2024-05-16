49fe392293dff1ff406cdedda06e5574906377b0
Abuddabi <web.abudabi@gmail.com>
2024-05-15 21:54:26 -0500

    password hash

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
 const { passwordPattern } = require("../utilities/account-validation")
 
 async function buildLogin(req, res) {
@@ -29,11 +30,28 @@ async function registerAccount(req, res) {
   const nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+  const regVars = {
+    title: "Registration",
+    nav,
+    passwordPattern,
+    errors: null,
+  }
+
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/registration", regVars)
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -42,19 +60,12 @@ async function registerAccount(req, res) {
       `Congratulations, you\'re registered ${account_firstname}. Please log in.`
     )
     res.status(201).render("account/login", {
-      title: "Login",
-      nav,
-      passwordPattern,
-      errors: null
+      ...regVars,
+      title: "Login"
     })
   } else {
     req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
-      title: "Registration",
-      nav,
-      passwordPattern,
-      errors: null
-    })
+    res.status(501).render("account/registration", regVars)
   }
 }
 
